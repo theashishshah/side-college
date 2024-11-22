5bc9a6a113f16a8a27581856e5f1bc9fdc71058c
Ashish Shah <eiepixxstuff@gmail.com>
2024-11-22 13:37:32 +0530

    create dbconnect file to the codebase

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already database is connected");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+    console.log("What we get after connecting the database", db);
+    console.log(db.connections);
+
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.log("Database connection failed", error);
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
+
+// The readyState is a number indicating the state of the MongoDB connection:
+// 0: disconnected
+// 1: connected
+// 2: connecting
+// 3: disconnecting
