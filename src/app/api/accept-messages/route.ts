26e1475663a65f5c24be8859c4d44cdb41a6ee2b
Ashish Shah <eiepixxstuff@gmail.com>
2024-12-13 04:17:29 +0530

    03 minor bug fix

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -66,7 +66,7 @@ export async function POST(request: Request) {
 }
 
 // function to check isAcceptingMessage
-export async function GET(request: Request) {
+export async function GET() {
   await dbConnect();
   console.log("Db is connected in accept message route in GET function.");
 
@@ -106,7 +106,7 @@ export async function GET(request: Request) {
       { status: 200 }
     );
   } catch (error) {
-    // console.log("Error getting is accepting message status.", error);
+    console.log("Error getting is accepting message status.", error);
     return Response.json(
       {
         success: false,
